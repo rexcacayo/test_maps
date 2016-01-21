fdbc003621d5e46f71ef1b6d20f455eb96766696
rexcacayo <rflugaresir@hotmail.com>
2016-01-21 18:24:21 -0430

    framework
    creando carpeta de trabajo

---
+++ jquery-mobile/funciones.js
@@ -0,0 +1,6 @@
+//AMBIENTE DE TRABAJO
+//trabajando en local
+//var dir= "http://127.0.0.1/mobile/ifa/php/";
+//trabajando en servidor'
+//var dir = "http://listamercado.ricardolugaresi.com.ve/php/"
+////////////////////////////////////////////////////////
