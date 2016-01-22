13b543774c6d9e684deeb79f55a5e4626ebd005e
rexcacayo <rflugaresir@hotmail.com>
2016-01-22 07:32:05 -0430

    plugin
    instalacion

--- jquery-mobile/funciones.js
+++ jquery-mobile/funciones.js
@@ -4,3 +4,14 @@
 //trabajando en servidor'
 //var dir = "http://listamercado.ricardolugaresi.com.ve/php/"
 ////////////////////////////////////////////////////////
+function initMap() {
+  // Create a map object and specify the DOM element for display.
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    scrollwheel: false,
+    zoom: 8
+  });
+	$('#map1').html(map);
+	$('#map1').trigger('create');
+	$.mobile.changePage("#mapas", {transition: "slideup"});
+}
