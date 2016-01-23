eba44c183d13b2a321cceb3793e2dadb6a26acc7
rexcacayo <rflugaresir@hotmail.com>
2016-01-23 14:28:21 -0430

    cambios finales
    elimando todo adicional basura rutinas sin nada

--- js/funciones.js
+++ js/funciones.js
@@ -1,9 +1,3 @@
-//AMBIENTE DE TRABAJO
-//trabajando en local
-//var dir= "http://127.0.0.1/mobile/ifa/php/";
-//trabajando en servidor'
-//var dir = "http://listamercado.ricardolugaresi.com.ve/php/"
-////////////////////////////////////////////////////////
 function map1(){
 var map;
 $(document).ready(function(){
